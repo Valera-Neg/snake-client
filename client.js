1bf87d1aa694d9cc74343b3e9fe8d722c5562fb7
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-15 03:47:31 +0000

    registred event handler(massage 'successfui registration and Player name)

--- client.js
+++ client.js
@@ -2,11 +2,16 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '172.25.92.67',
+    host: 'localhost',
     port: 50541
   });
   //interpert inconing data as text
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server✨');
+    conn.write('Name: VAL');
+  });
   
   conn.on('data', (data) => {
   console.log('Server says: ', data);
